3caa748e2590f5ace600d08a818632ce2f17b202
rmorioka <kysn.rm@gmail.com>
2022-01-21 15:59:41 +0900

    ボタンを作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
+  const maxManaValue = 16;
+  const minManaValue = 0;
+  const manaValues: number[] = Array.apply(
+    null,
+    new Array(maxManaValue + 1)
+  ).map((v, i) => {
+    return minManaValue + i;
+  });
+
+  let card: number = 0;
+
+  const getCard = function (manaValue: number) {
+    card = manaValue;
+    console.log(card);
+  };
 </script>
 
-<main />
+<main>
+  <div>
+    {#each manaValues as manaValue}
+      <button on:click={() => getCard(manaValue)}>{manaValue}</button>
+    {/each}
+  </div>
+  {card}
+</main>
 
 <style>
 </style>
